e15c3044060ece8f31b57188cf0c1c1aedf9cf8e
Zoellner122 <r1997@live.nl>
2023-02-24 15:26:13 +0100

    Adding template for virtual router instances

---
+++ terraform/templates.tf
@@ -0,0 +1,23 @@
+resource "opennebula_virtual_router_instance_template" "vr-template" {
+  name = "virtual-router-template"
+  permissions = "642"
+  
+  cpu = "0.5"
+  memory = "1024"
+
+  context = {
+    dns_hostname = "yes"
+    network      = "YES"
+  }
+
+  graphics {
+    keymap = "en-us"
+    listen = "0.0.0.0"
+    type   = "VNC"
+  }
+
+  os {
+    arch = "x86_64"
+    boot = ""
+  }
+}
