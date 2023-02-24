22e29eb7fd906ae867cefd905c4ef821b6ddec45
Zoellner122 <r1997@live.nl>
2023-02-24 11:49:07 +0100

    Adding definitions for Terraform to manage images

--- terraform/images.tf
+++ terraform/images.tf
@@ -3,7 +3,7 @@ resource "opennebula_image" "almalinux_8" {
   description = "Image for AlmaLinux 8"
   datastore_id = 1
   persistent = false
-  path = "https://marketplace.opennebula.io/appliance/6ae0a5e8-21a0-47e7-ad18-707af04eed60"
+  path = "https://marketplace.opennebula.io/appliance/6ae0a5e8-21a0-47e7-ad18-707af04eed60/download/0"
 
   permissions = "644"
 }
@@ -13,7 +13,7 @@ resource "opennebula_image" "ubuntu_20_04" {
   description = "Image for Ubuntu 20.04 LTS"
   datastore_id = 1
   persistent = false
-  path = "https://marketplace.opennebula.io/appliance/f15ff51e-d3eb-42e7-96e6-a7088da1b877"
+  path = "https://marketplace.opennebula.io/appliance/f15ff51e-d3eb-42e7-96e6-a7088da1b877/download/0"
 
   permissions = "644"
 }
